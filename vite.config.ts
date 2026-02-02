1bba7b4969041f69667a036987243dea01d0a93c
Rosen Kolev <rosen.kolev@hotmail.com>
2026-02-02 19:16:20 +0200

    change build to use node 22

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig({
+  plugins: [
+    vue(),
 
-  return {
-    plugins: [
-      vue(),
-
-      // Only include DevTools if not Storybook
-      !process.env.STORYBOOK && vueDevTools(),
-    ].filter(Boolean),
-    base: env.APP_BASE_URL || '/',
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+    // Only include DevTools if not Storybook
+    !process.env.STORYBOOK && vueDevTools(),
+  ].filter(Boolean),
+  base: process.env.APP_BASE_URL || '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
 })
